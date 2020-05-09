7129e04b7d448a5a6c81bf7632eaf5f45f88ba8e
Alexander Dyuzhikov <alex.duzh@gmail.com>
2020-05-10 02:19:53 +0300

    Backend. Add authentication for activeadmin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 # Changed from ActionController::API for active admin
 class ApplicationController < ActionController::Base
+  private
+
+  def authenticate_admin_user!
+    authenticate_or_request_with_http_basic('Whatever') do |name, password|
+      name == ENV['ACTIVEADMIN_USER_NAME'] && password == ENV['ACTIVEADMIN_USER_PASSWORD']
+    end
+  end
 end
