4f3b5a2b22bba0d35993b9fa215fa1111ed830bc
Alexander Dyuzhikov <alex.duzh@gmail.com>
2020-05-09 16:51:42 +0300

    Backend. Add random sentence controller.

---
+++ app/controllers/api/v1/random_sentences_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class RandomSentencesController < Api::V1::BaseController
+      def show
+        random_sentence = Sentence.order('RANDOM()').first
+
+        return not_found_request unless random_sentence
+
+        render json: random_sentence, status: :ok
+      end
+    end
+  end
+end
