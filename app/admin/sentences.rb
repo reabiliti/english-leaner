f61a2abb1b18f6dbe67a5469a1432eec7a59278e
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2020-05-11 01:47:09 +0300

    Admin. Fixed unpermited params. (#24)

--- app/admin/sentences.rb
+++ app/admin/sentences.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Sentence do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :russian, :english
-  #
-  # or
-  #
+  permit_params :russian, :english
+
   # permit_params do
   #   permitted = [:russian, :english]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
