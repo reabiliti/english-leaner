b865e6b3d2719be97abc4243ec3a4f825f6da340
Alexander Dyuzhikov <alex.duzh@gmail.com>
2020-05-11 17:32:14 +0300

    Frontend. Change name of project.

--- frontend/src/components/molecules/Footer/Footer.js
+++ frontend/src/components/molecules/Footer/Footer.js
@@ -5,7 +5,7 @@ import './footer.scss'
 const Footer = () => (
   <footer className='footer'>
     <div className='text-center container'>
-      <span className='text-muted'>English Leaner &copy; 2020</span>
+      <span className='text-muted'>English Learner &copy; 2020</span>
     </div>
   </footer>
 )
